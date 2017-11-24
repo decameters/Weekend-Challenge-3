5d51259c91e0954ee522d36bcc2828c682e1ceb9
liz elton <eeeqqqeee@gmail.com>
2017-11-24 13:30:21 -0600

    GET working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,6 +5,7 @@ $(document).ready(onReady);
 function onReady(){
     console.log('JQ loaded');
     $('#addTaskButton').on('click', addTask);
+    getAllTasks()
 }
 
 function addTask(){
@@ -17,11 +18,42 @@ function addTask(){
         },
         success: function (response) {
             console.log('response', response);
-            // run next function here
+            getAllTasks();
         }
     })
 }
 
 function getAllTasks(){
-
+    $.ajax({
+        method: 'GET',
+        url: '/todo',
+        success: function (response) {
+            console.log('GET response', response);
+            $('#viewTasks').empty();
+            for (var i = 0; i < response.length; i++) {
+                var task = response[i];
+                var $newTask = $('<li>' + response[i].task + ' // '+ response[i].completed + ' // ' + '</li>');
+                var $deleteTaskButton = $('<li><button class="deleteButton>Delete Task</button></li>');
+                $deleteTaskButton.data('id', task.id);
+                $newTask.append($deleteTaskButton);
+                $('#viewTasks').append($newTask);
+                }
+            
+        }
+    })
 }
+
+function deleteTask(){
+    
+}
+
+/* this all needs to be a table... not sure about the taskAddInput
+since on this its already on the database... */
+
+// function appendTasksToDOM(task){
+    // var $newTaskListItem = $('<tr></tr>');
+
+    // var $taskAddInput = $('#taskAddIn').val();
+    // // $taskAddInput.val()
+    // $newTaskListItem.append($taskAddInput);
+// }
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -23,4 +23,23 @@ router.post('/', function(req, res){
     })
 })
 
+router.get( '/', function (req, res){
+    pool.connect(function (errorConnectingToDatabase, client, done){
+        if(errorConnectingToDatabase) {
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query('SELECT * FROM tasks ORDER BY id;', function(errorMakingQuery, result){
+                done();
+                if(errorMakingQuery){
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                }
+            })
+        }
+    }) 
+  });
+
 module.exports = router;
